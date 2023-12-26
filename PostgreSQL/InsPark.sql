c9b95aa42c2d37cfc7d30123496207972c619c1c
Tiphk <144125968+Tiphk@users.noreply.github.com>
2023-12-27 00:30:38 +0100

    Modifications des Drops des tables pour éviter les erreurs de dépendances et mise à jour des insert des packages

--- PostgreSQL/InsPark.sql
+++ PostgreSQL/InsPark.sql
@@ -3,13 +3,13 @@ INSERT INTO projeta4.users VALUES ('manon.gardin@gmail.com', '123', 'Manon', 'Ga
 
 --Learning Package
 INSERT INTO projeta4.learning_package (title_package, description_package, category, target_audience, difficulty_level)
-VALUES ('Package 1', 'Description du package 1', 'Catégorie A', 'Public cible du package 1', 1);
+VALUES ('Package 1', 'Description du package 1', 'Catégorie A', 'Public cible du package 1', 'Easy');
 INSERT INTO projeta4.learning_package (title_package, description_package, category, target_audience, difficulty_level)
-VALUES ('Package 2', 'Description du package 2', 'Catégorie B', 'Public cible du package 2', 2);
+VALUES ('Package 2', 'Description du package 2', 'Catégorie B', 'Public cible du package 2', 'Medium');
 INSERT INTO projeta4.learning_package (title_package, description_package, category, target_audience, difficulty_level)
-VALUES ('Package 3', 'Description du package 3', 'Catégorie C', 'Public cible du package 3', 3);
+VALUES ('Package 3', 'Description du package 3', 'Catégorie C', 'Public cible du package 3', 'Hard');
 INSERT INTO projeta4.learning_package (title_package, description_package, category, target_audience, difficulty_level)
-VALUES ('Package 4', 'Description du package 4', 'Catégorie A', 'Public cible du package 4', 1);
+VALUES ('Package 4', 'Description du package 4', 'Catégorie A', 'Public cible du package 4', 'Easy');
 
 --Learning Facts
 INSERT INTO projeta4.learning_fact (recto, verso, id_package) VALUES ('Coucou ca va ?', 'Oui et toi ?', 1);

c9162e49102861ba0e0f67e375f133978dcdcb60
Tiphk <144125968+Tiphk@users.noreply.github.com>
2023-12-27 00:57:54 +0100

    Conditions sur l'ajout des packages : certains champs optionnels d'autres non.

--- frontend/src/app/addnew-package/addnew-package.component.ts
+++ frontend/src/app/addnew-package/addnew-package.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-addnew-package',
@@ -17,38 +18,49 @@ export class AddnewPackageComponent {
   difficulty_level: string = '';
   packageAdded: boolean = false;
 
+
+  @ViewChild('packageForm') packageForm!: NgForm;
+
   insertPackage(){
-    const PackageData = {
-      title_package: this.title_package,
-      description_package: this.description_package,
-      category: this.category,
-      target_audience: this.target_audience,
-      difficulty_level: this.difficulty_level
-    };
 
-    this.http.post('/api/createpackage', PackageData)
-      .subscribe(
-        (response) => {
+    if (this.packageForm.form.valid) {
 
-          this.packageAdded = true;
+      console.log('Insert Package method called');
 
-          // Réinitialiser les champs après l'ajout
-          this.title_package = '';
-          this.description_package = '';
-          this.category = '';
-          this.target_audience = '';
-          this.difficulty_level = '';
+      const packageData = {
+        title_package: this.title_package,
+        description_package: this.description_package,
+        category: this.category,
+        target_audience: this.target_audience,
+        difficulty_level: this.difficulty_level
+      };
 
-          // Masquer le message après quelques secondes
-          setTimeout(() => {
-            this.packageAdded = false;
-          }, 3000);
+      this.http.post('/api/createpackage', packageData)
+        .subscribe(
+          (response) => {
+            this.packageAdded = true;
 
-          console.log(response);
-        },
-        (error) => {
-          console.error(error);
-        }
-      );
+            // Reset the form fields after adding
+            this.title_package = '';
+            this.description_package = '';
+            this.category = '';
+            this.target_audience = '';
+            this.difficulty_level = '';
+
+            // Hide the message after a few seconds
+            setTimeout(() => {
+              this.packageAdded = false;
+            }, 3000);
+
+            console.log(response);
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
+    } else {
+      // Form is invalid, do something (optional)
+      console.log('Insert Package not valid');
+    }
   }
 }

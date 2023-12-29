da30be1f8c1d2cb6ce99bf21a33191ce0af38617
Mattcoddman <matias.ottensen@edu.devinci.fr>
2023-12-29 03:06:29 +0100

    Voila un premier graphique de statistiques :-)

--- frontend/src/app/progress-statistics/progress-statistics.component.ts
+++ frontend/src/app/progress-statistics/progress-statistics.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import * as Highcharts from 'highcharts';
+import * as Highcharts from 'highcharts/highstock';
 
 @Component({
   selector: 'app-progress-statistics',
@@ -8,30 +8,79 @@ import * as Highcharts from 'highcharts';
 })
 export class ProgressStatisticsComponent  implements OnInit
 {
+  chartOptions: any;
+  highcharts:typeof Highcharts= Highcharts;
 
   constructor() { }
 
-  ngOnInit() {
-    const chartElement = document.getElementById('myChart');
-
-    if (chartElement) {
-      const myChart = new Highcharts.Chart({
-        chart: {
-          renderTo: chartElement
-        },
-        series: [{
-          type: 'waterfall',
-          data: [10, 20, 30, 40, 50]
-        }]
-      });
-    } else {
-      console.error("L'élément avec l'ID 'myChart' n'a pas été trouvé dans le DOM.");
-    }
-
-    //myChart.title.text = 'statistics graph';
-    //myChart.xAxis.title.text = 'x';
-    //myChart.yAxis.title.text = 'y';
+  ngOnInit()
+  {
+    this.barchart();
 
   }
+  barchart()
+  {
+    this.chartOptions =
+      {
+        chart:
+          {
+            type: 'column'
+          },
+        title:
+          {
+            text: 'PROGRESS STATISTICS'
+          },
+        subtitle:
+          {
+            text: 'What happened here in anki?'
+          },
+        xAxis:
+          {
+            categories:[
+              'Africa', 'America', 'Asia', 'Europe'
+            ]
+
+          },
+        credits:
+        {enabled : false
+
+        },
+        /*plotOptions:
+          {
+            series:
+              {
+              stacking:'normal'
+              },
+            bar:
+              {
+                dataLables:
+                  {
+                    enabled: true
+                  }
+              }
+          },*/
+          series: this.Chartdata
+
+      }
+
+  }
+
+
+  //highchartdata
+   Chartdata= [
+  {
+    name: 'Year 2000',
+    data:[631, 727, 3202, 721]
+  },
+  {
+    name: 'Year 2001',
+    data:[814, 841, 3714, 726]
+  },
+  {
+    name: 'Year 2002',
+    data:[1276, 1007, 4561, 1000]
+  }
+
+]
 
 }

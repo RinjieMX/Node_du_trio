ac0d4c8b66a03976891aac82f71c0077c5a4a151
Tiphk <144125968+Tiphk@users.noreply.github.com>
2023-12-31 00:54:51 +0100

    Finalisation page Statistiques sur les Packages

--- frontend/src/app/package-statistics/package-statistics.component.ts
+++ frontend/src/app/package-statistics/package-statistics.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import * as Highcharts from "highcharts/highstock";
 import { DbServiceService } from "../db-service.service";
 import { HttpClient } from "@angular/common/http";
+
 import { forkJoin } from 'rxjs';
 
 @Component({
@@ -17,15 +18,17 @@ export class PackageStatisticsComponent implements OnInit {
     chartOptions: any;
     highcharts: typeof Highcharts = Highcharts;
 
+    type_charts: string = '';
+
     constructor(private DbService: DbServiceService, private http: HttpClient) {
     }
 
     async ngOnInit() {
         console.log("test");
-        this.loadData();
+        this.loadData_default();
     }
 
-    loadData() {
+    loadData_default() {
         this.DbService.getAllPackages().subscribe((data: any[]) => {
 
             //on récupère tout les packages
@@ -33,17 +36,6 @@ export class PackageStatisticsComponent implements OnInit {
 
             const allCategories: string[] = ['Maths', 'English', 'History','Informatics','Geography','Philosophy' ,'Physics'];
 
-            /*
-            const categoryCounts: { [category: string]: number } = this.all_packages.reduce((acc, pack) => {
-                const category = pack.category;
-                acc[category] = (acc[category] || 0) + 1;
-                return acc;
-            }, {});
-
-            const categories: string[] = Object.keys(categoryCounts);
-            const counts: number[] = Object.values(categoryCounts);
-
-             */
 
             const categoryCounts: { [category: string]: number } = allCategories.reduce((acc, category) => {
                 acc[category] = 0;
@@ -87,6 +79,134 @@ export class PackageStatisticsComponent implements OnInit {
                 };
 
         })
+
+    }
+
+    loadData_target() {
+        this.DbService.getAllPackages().subscribe((data: any[]) => {
+
+            //on récupère tout les packages
+            this.all_packages = data;
+
+            const allTarget: string[] = ['Children', 'Adults', 'Eldery','Any'];
+
+
+            const targetCounts: { [category: string]: number } = allTarget.reduce((acc, target) => {
+                acc[target] = 0;
+                return acc;
+            }, {} as { [target: string]: number });
+
+            this.all_packages.forEach((pack) => {
+                const targets = pack.target_audience;
+                targetCounts[targets] += 1;
+            });
+
+
+            this.chartOptions = {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'DISTRIBUTION OF TARGET'
+                },
+                xAxis: {
+                    categories: allTarget
+                },
+                yAxis: {
+                    title: {
+                        text: 'How many instance per Target ?'
+                    }
+                },
+                series: [
+                    {
+                        name: 'Number of Target Audience',
+                        color: 'white',
+                        data: allTarget.map((target_audience: string) => ({
+                            y: targetCounts[target_audience],
+                            color: targetCounts[target_audience] <= 1 ? '#eb5b5b' :
+                                targetCounts[target_audience] > 1 && targetCounts[target_audience] < 5 ? '#ffb6c1' :
+                                    targetCounts[target_audience] >= 5 ? '#27ae60' :
+                                        '#27ae60'
+                        }))
+                    }
+                ]
+            };
+
+        })
+
+    }
+
+    loadData_difficulty() {
+        this.DbService.getAllPackages().subscribe((data: any[]) => {
+
+            //on récupère tout les packages
+            this.all_packages = data;
+
+            const allDifficulties: string[] = ['Easy', 'Medium', 'Hard','Undefined'];
+
+
+            const difficultyCount: { [category: string]: number } = allDifficulties.reduce((acc, level) => {
+                acc[level] = 0;
+                return acc;
+            }, {} as { [level: string]: number });
+
+            this.all_packages.forEach((pack) => {
+                const level = pack.difficulty_level;
+                difficultyCount[level] += 1;
+            });
+
+
+            this.chartOptions = {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'DISTRIBUTION OF DIFFICULTY'
+                },
+                xAxis: {
+                    categories: allDifficulties
+                },
+                yAxis: {
+                    title: {
+                        text: 'How many instance per Difficulty ?'
+                    }
+                },
+                series: [
+                    {
+                        name: 'Number of Difficulty Level',
+                        color: 'white',
+                        data: allDifficulties.map((difficulty_level: string) => ({
+                            y: difficultyCount[difficulty_level],
+                            color: difficultyCount[difficulty_level] <= 1 ? '#eb5b5b' :
+                                difficultyCount[difficulty_level] > 1 && difficultyCount[difficulty_level] < 5 ? '#ffb6c1' :
+                                    difficultyCount[difficulty_level] >= 5 ? '#27ae60' :
+                                        '#27ae60'
+                        }))
+                    }
+                ]
+            };
+
+        })
+
+    }
+
+    updateChart(): void {
+
+      //on lis la valeur dans le drop down
+        console.log(this.type_charts);
+
+        switch (this.type_charts) {
+
+            case 'Target':
+                this.loadData_target();
+                break;
+            case 'Difficulty':
+                this.loadData_difficulty();
+                break;
+            default: //par défault c'est category
+                this.loadData_default();
+                break;
+        }
 
     }
 }

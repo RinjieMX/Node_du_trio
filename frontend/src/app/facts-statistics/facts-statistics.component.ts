b00026e02372dd147db843919c1278e0fd121bc6
Mattcoddman <matias.ottensen@edu.devinci.fr>
2023-12-30 04:00:36 +0100

    graphique de statistiques updated

--- frontend/src/app/facts-statistics/facts-statistics.component.ts
+++ frontend/src/app/facts-statistics/facts-statistics.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import * as Highcharts from "highcharts/highstock";
 import { DbServiceService } from "../db-service.service";
+import { HttpClient } from "@angular/common/http";
 import { forkJoin } from 'rxjs';
 
 @Component({
@@ -17,10 +18,10 @@ export class FactsStatisticsComponent
 
 
   idPackage: number = 5;
-  numberOfFacts1: number = 0;
-  numberOfFacts2: number = 0;
-  numberOfFacts3: number = 0;
-  numberOfFacts4: number = 0;
+  numberOfFacts1: number = 1;
+  numberOfFacts2: number = 2;
+  numberOfFacts3: number = 3;
+  numberOfFacts4: number = 4;
   numberOfFacts: number = 0;
 
 
@@ -28,40 +29,64 @@ export class FactsStatisticsComponent
   chartOptions: any;
   highcharts:typeof Highcharts= Highcharts;
 
-  constructor(private DbService: DbServiceService) { }
+  constructor(private DbService: DbServiceService, private http: HttpClient) { }
 
 
-  ngOnInit()
-  {
-    this.barchart();
-    this.numberOfFacts1 = this.getNumberOfFactsInPackage(1);
-    /*this.numberOfFacts2 = this.getNumberOfFactsInPackage(2);
-    this.numberOfFacts3= this.getNumberOfFactsInPackage(3);
-    this.numberOfFacts4 = this.getNumberOfFactsInPackage(4);*/
+
+  async ngOnInit() {
+    console.log(101010);
+
+    const responses = await forkJoin([
+      this.getNumberOfFactsInPackage(1),
+      this.getNumberOfFactsInPackage(2),
+      this.getNumberOfFactsInPackage(3),
+      this.getNumberOfFactsInPackage(4)
+    ]).toPromise();
 
 
+    if (responses ) {
+      this.numberOfFacts1 = responses[0];
+      this.numberOfFacts2 = responses[1];
+      this.numberOfFacts3 = responses[2];
+      this.numberOfFacts4 = responses[3];
+
+      this.Chartdata[0].data[0] = this.numberOfFacts1;
+      this.Chartdata[0].data[1] = this.numberOfFacts2;
+      this.Chartdata[0].data[2] = this.numberOfFacts3;
+      this.Chartdata[0].data[3] = this.numberOfFacts4;
+      console.log(this.Chartdata[0]);
+
+      this.barchart();
+      console.log(this.Chartdata[0]);
+    } else {
+      console.error('Réponses invalides:', responses);
+    }
   }
 
-  getNumberOfFactsInPackage(idPackage: number): number {
 
-    this.DbService.getNbFactInPackage(idPackage).subscribe(
-      (data: any) => {
-        this.numberOfFacts = data.count;
+  Chartdata= [
+    {
+      name: 'NUMBER OF FACTS',
+      /*data:[ 10, 12 , 17 ,30]*/
+      data:[ this.numberOfFacts1 , this.numberOfFacts2 , this.numberOfFacts3 ,this.numberOfFacts4]
+    }
 
-        this.numberOfFacts1 = this.getNumberOfFactsInPackage(1);
-        this.numberOfFacts2 = this.getNumberOfFactsInPackage(2);
-        this.numberOfFacts3 = this.getNumberOfFactsInPackage(3);
-        this.numberOfFacts4 = this.getNumberOfFactsInPackage(5);
+  ]
+  async getNumberOfFactsInPackage(idPackage: number): Promise<number> {
+    try {
+      const response = await this.http.get<{ count?: number }>(`/api/getNbFactLeft/${idPackage}`).toPromise();
 
-        //console.log('Nombre de faits dans le package :', this.numberOfFacts);
-      },
-      (error) => {
-        this.numberOfFacts= -1;
-        console.error('Erreur lors de la récupération du nombre de faits dans le package :', error);
+      if (response && response.count !== undefined) {
+        return response.count;
+      } else {
+        // Gérer le cas où la réponse n'a pas la propriété attendue
+        console.error('Réponse invalide:', response);
+        return 0;
       }
-    );
-
-    return this.numberOfFacts;
+    } catch (error) {
+      console.error('Erreur lors de la récupération du nombre de facts :', error);
+      return 0;
+    }
   }
 
 
@@ -110,20 +135,16 @@ export class FactsStatisticsComponent
           },*/
         series: this.Chartdata
 
+
       }
+
+    console.log(this.numberOfFacts1);
 
   }
 
 
   //highchartdata
-  Chartdata= [
-    {
-      name: 'NUMBER OF FACTS',
-      data:[this.DbService.getNbFactInPackage(1),
-            10,10,140]
-    }
 
-  ]
 
 
 

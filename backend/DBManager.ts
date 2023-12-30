4f393729dfc8bd4a030082f491c03ef609b5b83e
manon <manon.gardin@gmail.com>
2023-12-30 22:26:39 +0100

    Suppression de la partie User/Login sur SQL

--- backend/DBManager.ts
+++ backend/DBManager.ts
@@ -1,4 +1,4 @@
-import {Sequelize, Model, DataTypes } from 'sequelize';
+import { Sequelize, Model, DataTypes } from 'sequelize';
 
 // Configuration de Sequelize
 const sequelize = new Sequelize('LearningFactDb', 'learningdbUser', 'projeta4', {

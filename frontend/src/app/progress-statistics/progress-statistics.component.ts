0b173d68f6cd7eddc15eb031482e5e13da437e39
Tiphk <144125968+Tiphk@users.noreply.github.com>
2023-12-31 03:03:47 +0100

    Clean Code Statistiques + remaniement couleurs

--- frontend/src/app/progress-statistics/progress-statistics.component.ts
+++ frontend/src/app/progress-statistics/progress-statistics.component.ts
@@ -13,43 +13,12 @@ export class ProgressStatisticsComponent  implements OnInit
 {
   chartOptions: any;
   highcharts:typeof Highcharts= Highcharts;
-  idPackage = 1; // Remplacez par l'ID du package souhaité
-  apiUrl = `/api/getactualfactfrompackage/${(this.idPackage)}`;
+
   public learningFacts: any[] = [];
-  //public learningFacts: any;
-  packageId:number=4 ;
-  public tableau1: any[]= [];
-  public tableau2: any[]= [];
-  public tableau3: any[]= [];
-  public tableau4: any[]= [];
   public AllPackages: any;
 
-
-  async getApiResponse(packagenumber: number): Promise<any[]> {
-    try {
-      const response = await this.httpClient.get(`/api/getactualfactfrompackage/${packagenumber}`).toPromise();
-      const tab = response as any[];
-      //console.log(tab[0]);
-      return tab;
-    } catch (error) {
-      console.error('Erreur lors de la requête API :', error);
-      throw error; // Rejeter l'erreur pour qu'elle soit gérée par l'appelant
-    }
-  }
-
   constructor(private DbService: DbServiceService,private httpClient: HttpClient) { }
-  showdata(): void
-  {
-    console.log('showdatarunning');
-    console.log(this.tableau1);
 
-
-  }
-
-  /*getpackaenumer():number
-  {
-   this.DbService.getAllPackages()
-  }*/
   async ngOnInit() {
     console.log('Début de ngOnInit');
       this.loadData();
@@ -151,7 +120,7 @@ export class ProgressStatisticsComponent  implements OnInit
                   },
                 subtitle:
                   {
-                    text: 'what was easy ? dificult?'
+                    text: 'What was easy ? Difficult?'
                   },
                 xAxis:
                   {
@@ -170,92 +139,5 @@ export class ProgressStatisticsComponent  implements OnInit
       );
     });
   }
-
-
-
-
-
-  /*
-  console.log('Avant la requête HTTP');
-
-  this.httpClient.get(`/api/getactualfactfrompackage/${this.packageId}`).subscribe(
-    (response) => {
-      console.log('Réponse de l\'API réussie :', response);
-      this.learningFacts = response as any;
-      console.log(this.learningFacts[0].state_fact);
-      for (let i = 0; i < this.learningFacts.length; i++) {
-        let fact = this.learningFacts[i];
-        console.log(`Fact ${fact.id_fact}: ${fact.state_fact}`);
-      }
-
-    },
-    (error) => {
-      console.error('Erreur lors de la requête API :', error);
-    }
-  );*/
-
-
-
-
-  numeroaprint:number=10;
-  barchart()
-  {
-    this.chartOptions =
-      {
-        chart:
-          {
-            type: 'column'
-          },
-        title:
-          {
-            text: 'PROGRESS IN ANKI'
-          },
-        subtitle:
-          {
-            text: 'what was easy? dificult?'
-          },
-        xAxis:
-          {
-            categories:[
-              'Maths', 'python', 'geography', 'english'
-            ]
-
-          },
-        credits:
-          {enabled : false
-
-          },
-
-        series: this.Chartdata
-
-      }
-
-  }
-
-
-  //highchartdata
-  Chartdata= [
-    {
-      name: 'Easy',
-      data:[2, 3, 1, 8],
-      color: '#27ae60'
-    },
-    {
-      name: 'correct',
-      data:[4, 1, 3, 1],
-      color: '#89e0ae'
-    },
-    {
-      name: 'To review',
-      data:[1, 7, 1, 1],
-      color: '#ffb6c1'
-    },
-    {
-      name: 'difficult',
-      data:[1, 0, 2, 3],
-      color: '#f06c84'
-    }
-
-  ]
 
 }
